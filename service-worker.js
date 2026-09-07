7393acf393233d4e591c5ea56379261c5a763e64
Yician0830 <jich6625@gmail.com>
2026-09-07 14:03:44 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
 /* 推し手帳 - 極簡 Service Worker
    只快取 App 外殼（HTML / manifest / icons），讓已開過的頁面在離線或訊號不穩時仍能打開。
    Firebase 等雲端資料仍需要網路，不在快取範圍內。 */
-const CACHE_NAME = 'oshi-app-shell-v1';
+const CACHE_NAME = 'oshi-app-shell-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
+  './favicon.ico',
+  './icons/favicon-16.png',
+  './icons/favicon-32.png',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-512-maskable.png',
